da72adbce8a188aa6bd6a316aa2a1672af555d1d
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-01-27 18:47:36 -0600

    Add files via upload

---
+++ bookdatatypeex.py
@@ -0,0 +1,21 @@
+# Python program to demonstrate different data types
+
+# Literals
+x = 10
+print("Literal:", x)
+
+# Int
+y = -20
+print("Integer:", y)
+
+# Float
+z = 15.5
+print("Float:", z)
+
+# Str
+name = "John"
+print("String:", name)
+
+# Bool
+is_active = True
+print("Boolean:", is_active)
