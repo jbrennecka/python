12e107bb72c87ee7bcc608ad43b0ddc94e7aecb7
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-01-27 18:31:28 -0600

    Add files via upload

---
+++ bookprintex.py
@@ -0,0 +1,12 @@
+#simple knock-knock joke, formatted, multiple statements
+
+print("knock knock,")
+print("\t\t\t\twhos there?")
+print("beet thah,")
+print("\t\t\t\tbeet thah who?")
+print("that's antisemetic")
+
+
+#simple knock-knock joke, formatted, one statement, one line
+
+print("knock knock,\n\t\t\t\t", "whos there?\n", "beet thah,\n\t\t\t\t", "beet thah who?\n", "that's antisemetic")
