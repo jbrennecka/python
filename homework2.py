dca664c7da7d066cb443aafc5cfb66b522cc44e1
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-02-03 21:29:06 -0600

    Update homework2.py
    Noticed from the other assignment how to make the code smaller, this still runs perfect.

--- homework2.py
+++ homework2.py
@@ -10,27 +10,22 @@ isn't 219.0
 '''
 
 def inches_to_cm(inch):
-    inch = 2
     x = round((inch * 2.54), 2)
     return x
 
 def feet_to_meters(ft):
-    ft = 3
     y = round((ft * 0.3048), 2)
     return y
 
 def lbs_to_kg(lbs):
-    lbs = 2.7
     z = round((lbs / 2.20462), 2)
     return z
 
 def hours_to_minutes(hrs):
-    hrs = 2.5
     mins = hrs * 60
     return mins
 
 def hours_to_minutes2(hrs):
-    hrs = 3.65
     mins = hrs * 60
     return mins
 
