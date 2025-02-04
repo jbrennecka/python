9d422c7b14166964ab7b1a3a7394dd31a62f1348
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-02-03 19:45:27 -0600

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,7 +1,7 @@
 '''
 Homework2
 Name: Jesse Brennecka
-github link:
+github link: https://github.com/jbrennecka/python/blob/main/homework2.py
 
 PERSONAL COMMENT: I had to modify the doctest2.py file because the file itself had 
 two different instances of hours to minutes conversion, and every time I would test 
