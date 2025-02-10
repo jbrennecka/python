b7d3a311c18b5f36292f165396d003998380b565
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-02-09 23:15:01 -0600

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,30 @@
+'''
+Homework5
+Name: Jesse Brennecka
+github link: 
+'''
+
+def collatz_conjecture(num):
+    print(float(num))
+    while num != 1.0:
+        if num % 2 == 0:
+            num = num / 2
+            print(num)
+        else:
+            num = num * 3 + 1
+            print(num)
+    return
+
+def add_numbers(num):
+    x = 1
+    y = 0
+    while x < num:
+        y = y + x
+        x = x + 1
+        
+    print(y)
+    return
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest5.py'))
