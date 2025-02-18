f78604e040c3512e483740e3f943b8c4ff7b6c91
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-02-17 22:23:02 -0600

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,39 @@
+'''
+Homework10
+Name:
+github link:
+'''
+
+
+def find_missing_number(lst):
+    x = 0
+    llst = lst[:]
+    llst.sort()
+    nlst = []
+    for i in llst:
+        while x <= i:
+            if x in llst:
+                x += 1
+            else:
+                nlst.append(x)
+                x += 1
+            
+    print(nlst)
+    return 
+
+def even_partition(lst):
+    x = 0
+    xlst = lst[:]
+    ylst = []
+    for x in xlst:
+        if x % 2 == 0:
+            ylst.append(x)
+            x += 1
+        else:
+            x += 1
+    print(ylst)
+    return
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest10.py'))
