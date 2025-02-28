ab0c8eb183f7b8272a1375bbc1a8380ff301ecd1
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-02-27 18:53:01 -0600

    Add files via upload

---
+++ homework12.py
@@ -0,0 +1,20 @@
+'''
+Homework12
+Name: Jesse Brennecka
+github link:
+'''
+
+def rectangle(w, h):
+    a = w * h
+    print('\'The area of the rectangle is', str(a), 'square units\'')
+
+def circle(rad):
+    a = 3.14 * rad * rad
+    format_a = f"{a:.2f}"
+    format_a = float(format_a)
+    format_a = f"{format_a:g}"
+    print('\'The area of a circle is', str(format_a), 'square units\'')
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest12.py'))
