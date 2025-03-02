df4df89e98ad882b9762314fa4b464abde520bd7
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-03-01 19:42:29 -0600

    Update homework13.py

--- homework13.py
+++ homework13.py
@@ -1,7 +1,7 @@
 '''
 Homework13
 Name: Jesse Brennecka
-github link:
+github link: https://github.com/jbrennecka/python/blob/main/homework13.py
 '''
 def calculate_interest(principal, rateInt, time):
     simpInt = int((principal * rateInt * time))
