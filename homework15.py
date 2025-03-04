cfa865801c13737614d4f4b08668551fb6993177
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-03-03 19:21:43 -0600

    Add files via upload

---
+++ homework15.py
@@ -0,0 +1,24 @@
+'''
+Homework15
+Name: Jesse Brennecka
+github link:
+'''
+
+def factorial(n):
+    if n != 0 or n != 1:
+        m = 1
+        for x in range(1, n+1):
+            m *= x
+        return m    
+    else:
+        return 1
+
+def main(num1):
+    
+    ans = factorial(num1)
+    print("\"The factorial of " + str(num1) + " is " + str(ans) + ".\"")
+    return
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest15.py'))
