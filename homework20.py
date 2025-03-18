468e23431c59c83f54906795ef6f66a0ba626d98
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-03-17 20:29:37 -0500

    Add files via upload

---
+++ homework20.py
@@ -0,0 +1,29 @@
+'''
+Homework20
+Name:   Jesse Brennecka
+github link: 
+'''
+
+def convert_to_ascii(string):
+    if len(string) == 1:
+        print(ord(str(string)))
+    else:
+        lst = []
+        for i in string:
+            lst.append(ord(str(i)))
+        print(lst)
+    
+def filter_non_ascii(string):
+    if len(string) == 1:
+        if str(string).isprintable():
+            print(string)
+    else:
+        p = ""
+        for i in str(string):
+            if ord(i) < 126 and ord(i) > 31:
+                p += i
+        print(p)    
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest20.py'))
