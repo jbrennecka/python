df4cd18dc35d329299e8b4c1f95f96673fe161dd
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-03-17 21:09:00 -0500

    Add files via upload

---
+++ homework22.py
@@ -0,0 +1,28 @@
+'''
+Homework22
+Name:   Jesse Brennecka
+github link: 
+'''
+def mask_creditcard(string):
+    card = ""
+    count = 0
+    for i in str(string):
+        count += 1
+        if count <= 12:
+            card += "*"
+        else:
+            card += i
+    print('\'' + card + '\'')
+    
+def remove_vowels(string):
+    vowels = "aeiouAEIOU"
+    sent = ""
+    for i in str(string):
+        if i not in vowels:
+            sent += i
+    print('\'' + sent + '\'')
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testfile('doctest22.py'))
