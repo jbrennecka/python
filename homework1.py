54335cf435bcf3845e2fb1dbe561ab7c0e51744f
Jesse Brennecka <54329683+jbrennecka@users.noreply.github.com>
2025-04-07 21:50:56 -0500

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,32 +1,57 @@
 '''
 Homework1
-Name: Jesse Brennecka
+Name:
 github link:
+Note: Remember to use comments for each function.
+doc strings should include what each input consists of, 
+what the expected output is and a description of the function.
 '''
-def add(num1, num2):
-    ans1 = num1 + num2
-    print(ans1)
-def subtract(num1, num2):
-    ans2 = num1 - num2
-    print(ans2)
-def multiply(num1, num2):
-    ans3 = num1 * num2
-    print(ans3)
-def division(num1, num2):
-    ans4 = num1 / num2
-    print(ans4)
-def int_div(num1, num2):
-    ans5 = num1 // num2
-    print(ans5)
-def modulus(num1, num2):
-    ans6 = num1 % num2
-    print(ans6)
-def exp(num1, num2):
-    ans7 = num1 ** num2
-    print(ans7)
-def area(length, width):
-    ans8 = length * width
-    print("'The area of the rectangle with a length of 3 and a width of 4 is", ans8, end="'")
+
+class Bank_Account:
+    def __init__(self,name,starting_amount):
+        self.name = name
+        self.account = starting_amount
+        
+    
+    def __repr__(self):
+        return f"Bank_Account(name='{self.name}', Account Balance={self.account})"
+    
+    def __str__(self):
+        pod = 'Account Name: ' + self.name +'\n'+'Account Balance: ' + str(self.account)
+        return pod
+    
+    def deposit(self,amount):
+
+        # Code segment will deposit "amount" into account after a check if it is positive (more than zero)
+
+        deposit_amount = amount
+        if deposit_amount > 0:
+            self.account += deposit_amount
+            print(str(deposit_amount) + ' deposited. New balance: ' + str(self.account))
+        if deposit_amount < 0:
+            print('Please deposit a positive number.')
+        
+    
+    def withdraw(self,amount):
+
+        # Code segment will withdraw "amount" from account if both the account has enough funds and that it is positive(more than zero)
+
+        withdraw_amount = amount
+        start_amount = self.account
+        if withdraw_amount > start_amount:
+            print('Insufficient funds.')
+        else:
+            if withdraw_amount > 0:
+                self.account -= withdraw_amount
+                print(str(withdraw_amount) + ' withdrawn. New balance: ' + str(self.account))
+        if withdraw_amount < 0:
+            print('Please withdraw an amount greater than zero.')
+        
+
+    
+    def check_balance(self):
+        print('Balance: ' + str(self.account))
+
 if __name__ == "__main__":
     import doctest
     print(doctest.testfile('doctest1.py'))
